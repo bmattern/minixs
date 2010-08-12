936fe31b6896339ed12fa16c188b02c393678e64
Brian Mattern <rephorm@rephorm.com>
2010-08-12 15:49:18 -0700

    update to combined code and ask before overwriting

--- process_cerh3.py
+++ process_cerh3.py
@@ -1,25 +1,35 @@
-import calibrate, process, exposure
+import minixs
 import time
-import os
+import os, sys
 
+######################################
+# Configurable parameters
 ######################################
 
 # dispersive direction in image
-direction = calibrate.HORIZONTAL
+direction = minixs.HORIZONTAL
 
+# calibration parameters
 calib_root = 'cerh3/cerh3-elastic4_'
 calib_nums = range(1,17)
 calib_scan = 'cerh3/cerh3elastic.0004'
 
-calib_filename = 'cerh3/cerh3_calibration.dat'
+calib_scan_energy_column = 0
+calib_scan_I0_column = 6
 
 calib_filter_low  = 3.5
 calib_filter_high = 75
 calib_filter_neighbors = 1
 
+calib_filename = 'cerh3/cerh3_calibration.dat'
+
+# spectrum parameters
 spec_root = 'cerh3/cerh3-rxes4_'
 spec_nums = range(1,79)
 spec_scan = 'cerh3/cerh3rexs4.0002'
+
+spec_scan_energy_column = 0
+spec_scan_I0_column = 6
 
 spec_filter_low = 0
 spec_filter_high = 200
@@ -33,14 +43,37 @@ spec_filename = 'cerh3/cerh3_rixs.dat'
 
 #####################################
 
-if not os.path.exists(calib_filename):
-  calib_files = calibrate.gen_image_names(calib_root, calib_nums)
-  energies, I0s = calibrate.read_scan_info(calib_scan)
+def ask_yn_question(question):
+  ret = False
+  valid = False
+  while not valid:
+    sys.stdout.write(question)
+    response = sys.stdin.readline().strip().lower()
+    if response[0] == 'y':
+      valid = True
+      ret = True
+    elif response[0] == 'n':
+      valid = True
+      ret = False
+    else:
+      print "I didn't understand your response. Please try again."
+  return ret
+
+
+do_calibration = True
+
+if os.path.exists(calib_filename):
+  do_calibration = ask_yn_question("Calibration matrix exists. Recalibrate? (y/n): ")
+      
+if do_calibration: 
+  calib_files = minixs.gen_file_list(calib_root, calib_nums)
+  energies, I0s = minixs.read_scan_info(calib_scan,
+      [calib_scan_energy_column, calib_scan_I0_column])
 
   print "Calibration"
   t1 = time.time()
 
-  c = calibrate.Calibrate(energies, I0s, calib_files, direction)
+  c = minixs.Calibrator(energies, I0s, calib_files, direction)
   c.filter_images(calib_filter_low, calib_filter_high, calib_filter_neighbors)
   c.build_calibration_matrix()
   c.kill_regions([])
@@ -50,29 +83,36 @@ if not os.path.exists(calib_filename):
   t2 = time.time()
   print "  finished in %.2f s"  % (t2 - t1,)
 else:
-  print "Already calibrated, skipping."
+  print "Skipping calibration."
 
-energies, I0s = calibrate.read_scan_info(spec_scan)
+do_process = True
 
-print "Process spectra"
-t1 = time.time()
+if os.path.exists(spec_filename):
+  do_process = ask_yn_question("Processed data file exists. Reprocess? (y/n): ")
 
-spectra = process.process_all(
-    calib_filename,
-    spec_scan,
-    spec_root,
-    spec_nums,
-    low_cutoff=spec_filter_low,
-    high_cutoff=spec_filter_high,
-    low_energy=spec_low_energy,
-    high_energy=spec_high_energy,
-    energy_step=spec_energy_step
-    )
+energies, I0s = minixs.read_scan_info(spec_scan,
+    [spec_scan_energy_column, spec_scan_I0_column])
 
-t2 = time.time()
-print"  finished in %.2f s" % (t2 - t1,)
+if do_process:
+  print "Process spectra"
 
-rixs = process.build_rixs(spectra, energies)
-process.save_rixs(spec_filename, rixs)
+  t1 = time.time()
 
+  spectra = minixs.process_all(
+      calib_filename,
+      spec_scan,
+      spec_root,
+      spec_nums,
+      low_cutoff=spec_filter_low,
+      high_cutoff=spec_filter_high,
+      low_energy=spec_low_energy,
+      high_energy=spec_high_energy,
+      energy_step=spec_energy_step
+      )
+
+  t2 = time.time()
+  print"  finished in %.2f s" % (t2 - t1,)
+
+  rixs = minixs.build_rixs(spectra, energies)
+  minixs.save_rixs(spec_filename, rixs)
 

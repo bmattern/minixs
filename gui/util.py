fc8f943ad7f02cad6fbc19792ec4b22efe35d67c
Brian Mattern <rephorm@rephorm.com>
2010-09-28 11:14:14 -0700

    don't consider adjacent xtals as intersecting

--- gui/util.py
+++ gui/util.py
@@ -29,8 +29,6 @@ def xtals_intersect(xtal1, xtal2):
   (ax1, ay1), (ax2, ay2)  = xtal1
   (bx1, by1), (bx2, by2)  = xtal2
 
-  print "intersect: ", xtal1, "\n", xtal2
-
-  return (ax1 <= bx2 and bx1 <= ax2 and 
-      ay1 <= by2 and by1 <= ay2)
+  return (ax1 < bx2 and bx1 < ax2 and 
+      ay1 < by2 and by1 < ay2)
   

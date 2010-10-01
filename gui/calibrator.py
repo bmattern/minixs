cdd537d7bfdb3b6e566ab8d368d02e0c3e89dfe5
Brian Mattern <rephorm@rephorm.com>
2010-10-01 13:04:08 -0700

    move ids, etc to separate file

--- gui/calibrator.py
+++ gui/calibrator.py
@@ -8,44 +8,13 @@ import util
 from frame import MenuFrame
 from matplotlib import cm, colors
 
+from calibrator_const import *
+
 from image_view import ImageView, EVT_COORDS
 from image_tools import RangeTool, Crosshair, EVT_RANGE_CHANGED, EVT_RANGE_ACTION_CHANGED
 
 HPAD = 10
 VPAD = 5
-
-ID_MAIN_FRAME       = wx.NewId()
-ID_DATASET_NAME     = wx.NewId()
-ID_EXPOSURE_LIST    = wx.NewId()
-ID_READ_ENERGIES    = wx.NewId()
-ID_SELECT_EXPOSURES = wx.NewId()
-ID_APPEND_ROW       = wx.NewId()
-ID_DELETE_ROW       = wx.NewId()
-ID_CLEAR_ENERGIES   = wx.NewId()
-ID_CLEAR_EXPOSURES  = wx.NewId()
-ID_DISPERSIVE_DIR   = wx.NewId()
-ID_EXPOSURE_SLIDER  = wx.NewId()
-ID_CALIBRATE        = wx.NewId()
-ID_FILTER_EMISSION  = wx.NewId()
-ID_IMAGE_PANEL      = wx.NewId()
-
-ID_VIEW_TYPE        = wx.NewId()
-ID_SHOW_XTALS       = wx.NewId()
-
-ID_IMPORT_XTALS     = wx.NewId()
-ID_EXPORT_XTALS     = wx.NewId()
-
-ID_LOAD_SCAN        = wx.NewId()
-
-WILDCARD_CALIB = "Calibration Files (*.calib)|*.calib|Data Files (*.dat)|*.dat|Text Files (*.txt)|*.txt|All Files|*"
-WILDCARD_EXPOSURE = "TIF Files (*.tif)|*.tif|All Files|*"
-WILDCARD_XTAL = "Crystal Files (*.xtal)|*.xtal|Calibration Files (*.calib)|*.calib|Text Files (*.txt)|*.txt|All Files|*"
-WILDCARD_XTAL_EXPORT = "Crystal Files (*.xtal)|*.xtal"
-WILDCARD_SCAN = "Scan Files (*.nnnn)|*.????|Text Files (*.txt)|*.txt|All Files|*"
-
-
-STATUS_COORDS  = 0
-STATUS_MESSAGE = 1
 
 class CalibratorModel(mxinfo.CalibrationInfo):
   def __init__(self):

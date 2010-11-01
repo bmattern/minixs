afe7b48c9fea66df7ab78cb913769f971f62640c
Brian Mattern <bmattern@uw.edu>
2010-10-31 23:23:26 -0700

    fix up exposure filters

--- filter.py
+++ filter.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+"""
+Exposure filters
+"""
+
 #########################
 #                       #
 # Abstract Base Classes #
@@ -12,35 +16,43 @@ class Filter(object):
   Filters take an array of pixels and process them in some fashion
   """
   name = ""
+  view_name = "UnimplementedView"
   def __init__(self):
     self.enabled = True
 
-  def filter(pixels, energy):
-    pass
+  def filter(self, pixels, energy):
+    raise ValueError("Unimplemented Filter: %s" % self.name)
 
   def set_str(self, valstr):
     return None
 
-  def get_str(self, val):
+  def get_str(self):
     return None
 
-class DummyFilter(Filter):
-  pass
+  def set_val(self, val):
+    self.val = val
+
+  def get_val(self):
+    return self.val
 
 class IntFilter(Filter):
+  view_name = "IntFilterView"
+
   def set_str(self, valstr):
     if valstr == "None":
       self.val = None
     else:
       self.val = int(valstr)
 
-  def get_str(self, val):
+  def get_str(self):
     if self.val is None:
       return "None"
     else:
       return str(self.val)
 
 class FloatFilter(Filter):
+  view_name = "FloatFilterView"
+
   def set_str(self, valstr):
     if self.val is None:
       return "None"
@@ -50,42 +62,65 @@ class FloatFilter(Filter):
   def get_str(self):
     return "%.2f" % self.val
 
+class ChoiceFilter(Filter):
+  view_name = "ChoiceFilterView"
+
+  CHOICES = [
+      ]
+
+  def set_str(self, valstr):
+    try:
+      self.val = self.CHOICES.index(valstr)
+    except ValueError:
+      raise ValueError("Unknown %s Type: %s" % (self.name, valstr))
+
+  def get_str(self):
+    return self.CHOICES[self.val]
+
 ##################
 #                #
 # Actual Filters #
 #                #
 ##################
 
-class MinFilter(DummyFilter):
+class MinFilter(IntFilter):
   name = "Min Visible"
+  view_name = "IntFilterView"
 
-class MaxFilter(DummyFilter):
+  def filter(self, pixels, energy):
+    pass
+
+class MaxFilter(IntFilter):
   name = "Max Visible"
+  view_name = "IntFilterView"
+
+  def filter(self, pixels, energy):
+    pass
 
 class LowFilter(IntFilter):
   name = "Low Cutoff"
-  def filter(pixels, energy):
+  def filter(self, pixels, energy):
     if self.val is None:
       return
     pixels[np.where(pixels < self.val)] = 0
 
 class HighFilter(IntFilter):
   name = "High Cutoff"
-  def filter(pixels, energy):
+  def filter(self, pixels, energy):
     if self.val is None:
       return
     pixels[np.where(pixels > self.val)] = 0
 
 class NeighborFilter(IntFilter):
   name = "Neighbors"
-  def filter(pixels, energy):
+  def filter(self, pixels, energy):
     from itertools import product
     nbors = (-1,0,1)
     mask = np.sum([
-      np.roll(np.roll(self.pixels>0,i,0),j,1)
+      np.roll(np.roll(pixels > 0,i,0),j,1)
       for i,j in product(nbors,nbors)
       if i != 0 or j != 0
-      ], 0) >= cutoff
+      ], 0) >= self.val 
     pixels *= mask
 
 class BadPixelFilter(Filter):
@@ -104,7 +139,7 @@ class BadPixelFilter(Filter):
     else:
       raise Exception("Invalid filter value: %s" % valstr)
 
-  def get_str(self, val):
+  def get_str(self):
     pts = ';'.join(['%d,%d' % p for p in self.points])
     return '%d|%s' % (self.mode, pts)
 
@@ -117,41 +152,49 @@ class BadPixelFilter(Filter):
       elif self.mode == MODE_INTERP_V:
         pixels[y,x] = (pixels[y-1,x] + pixels[y+1,x])/2.
 
-class EmissionFilter(Filter):
+class EmissionFilter(ChoiceFilter):
+  name = "Emission Filter"
+
   TYPE_FE_KBETA = 0
-  TYPE_NAMES = [
+  CHOICES = [
       "Fe Kbeta"
       ]
   def filter(self, pixels, energy):
-    if self.type == self.TYPE_FE_KBETA:
-      #XXX encode these values in file valstr?
+    if self.val == self.TYPE_FE_KBETA:
       if energy >= 7090:
         z = 1.3214 * energy - 9235.82 - 12
         pixels[0:z,:] = 0
     else:
       raise ValueError("Unimplemented Emission Filter")
 
-  def set_str(self, valstr):
-    try:
-      self.type = self.TYPE_NAMES.index(valstr)
-    except ValueError:
-      raise ValueError("Unknown Emission Filter Type: %s" % valstr)
-
-  def get_str(self):
-    return self.TYPE_NAMES[self.type]
 
 ###################
 #                 #
 # Filter Registry #
 #                 #
 ###################
-FILTERS = [ MinFilter, MaxFilter, LowFilter, HighFilter, NeighborFilter, BadPixelFilter, EmissionFilter ]
+FILTERS = [
+  MinFilter,
+  MaxFilter,
+  LowFilter,
+  HighFilter,
+  NeighborFilter,
+  #BadPixelFilter,
+  EmissionFilter
+  ]
 
-filter_map = {}
+REGISTRY = []
+FILTER_MAP = {}
+
 for f in FILTERS:
-  filter_map[f.name] = f
+  FILTER_MAP[f.name] = f
+  REGISTRY.append(f)
 
 def get_filter_by_name(name):
-  global filter_map
-  return filter_map.get(name, None)
+  global FILTER_MAP
+  return FILTER_MAP.get(name, None)
 
+def register(f):
+  FILTER_MAP[f.name] = f
+  REGISTRY.append(f)
+  

b95468b70435a6be1232e01a827c243f25d05fa2
Brian Mattern <bmattern@uw.edu>
2010-11-01 15:42:13 -0700

    reorganize code and add (broken) ellipsoidal fit

--- calibrate2.py
+++ calibrate2.py
@@ -67,7 +67,11 @@ def find_maxima(pixels, direction, energy, window_size = 3):
   # return N x 2 array of peak locations
   return np.vstack([x,y,z]).T
 
-def fit_xtal(xtal, points, dest):
+FIT_QUADRATIC = 1
+FIT_QUARTIC   = 2
+FIT_ELLIPSOID = 3
+
+def fit_xtal(xtal, points, dest, fit_type = FIT_QUARTIC):
   # boundary coordinates
   (x1,y1),(x2,y2) = xtal
 
@@ -90,56 +94,84 @@ def fit_xtal(xtal, points, dest):
     print "Warning: No points in xtal ", xtal
     return
 
-  # fit to quadratic
-  #A = np.vstack([x**2, y**2, x*y, x, y, np.ones(x.shape)]).T
-  #fit, r = np.linalg.lstsq(A,z)[0:2]
-
-  # fit to 2d quartic using linear least squares
-  # XXX really this should use an ellipsoid
-  A = np.vstack([x**3,y**3,x**2*y,x*y**2,x**2, y**2, x*y, x, y, np.ones(x.shape)]).T
-  fit, r = np.linalg.lstsq(A,z)[0:2]
-
-  # fit ellipsoid
-  """
-  data = np.vstack([
-    x*x,
-    y*y,
-    z*z,
-    x*y,
-    y*z,
-    z*x,
-    x,
-    y,
-    z])
-
-  fit, r = np.linalg.lstsq(data,np.ones(x.shape))[0:2]
-  """
-
-  # append residues for this xtal to end of lists
-  rms_res = np.sqrt(r / len(z))[0]
-  lin_res = sum(z - np.dot(A, fit)) / len(z)
-
-  # evaluate fit at center of each pixel
+  # build points to evaluate fit at
   xx, yy = np.meshgrid(np.arange(x1,x2), np.arange(y1,y2))
   xx = np.ravel(xx)
   yy = np.ravel(yy)
 
-  zz = np.dot(np.vstack([xx**3,yy**3,xx**2*yy,xx*yy**2,xx**2,yy**2,xx*yy,xx,yy,np.ones(xx.shape)]).T,fit).T
-  """
-  A,B,C,D,E,F,G,H,I = fit
-  a = C
-  b = E*yy + F*xx + I
-  c = A*xx**2 + B*yy**2 + D*x*y + G*x + H*y - 1
-  zz = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
-  """
+  if fit_type == FIT_QUADRATIC:
+    # fit to quadratic: z = Ax^2 + By^2 + Cxy + Dx + Ey + F
+    A = np.vstack([x**2, y**2, x*y, x, y, np.ones(x.shape)]).T
+    fit, r = np.linalg.lstsq(A,z)[0:2]
 
+    # calculate residues
+    rms_res = np.sqrt(r / len(z))[0]
+    lin_res = sum(z - np.dot(A, fit)) / len(z)
+
+    # evaluate at all points
+    zz = np.dot(
+           np.vstack([xx**2,yy**2,xx*yy,xx,yy,np.ones(xx.shape)]).T,
+           fit
+         ).T
+
+  elif fit_type == FIT_QUARTIC:
+    # fit to quartic:
+    #   Ax^3 + By^3 + Cx^2y + Dxy^2 + Ex^2 + Fy^2 + Gxy + Hx + Iy + J = z
+    A = np.vstack([x**3,y**3,x**2*y,x*y**2,x**2, y**2, x*y, x, y, np.ones(x.shape)]).T
+    fit, r = np.linalg.lstsq(A,z)[0:2]
+
+    # calculate residues
+    rms_res = np.sqrt(r / len(z))[0]
+    lin_res = sum(z - np.dot(A, fit)) / len(z)
+
+    # evaluate at all points
+    zz = np.dot(
+           np.vstack([
+             xx**3, yy**3, xx**2*yy, xx*yy**2,
+             xx**2, yy**2, xx*yy,
+             xx, yy,
+             np.ones(xx.shape)
+           ]).T,
+           fit
+         ).T
+
+  elif fit_type == FIT_ELLIPSOID:
+    raise Exception("Fit method not yet implemented.")
+    # XXX this doesn't seem to work...
+    # Fit to ellipsoid:
+    # Ax^2 + By^2 + Cz^2 + Dxy + Eyz + Fzx + Gx + Hy + Iz = 1
+    data = np.vstack([
+      x*x,
+      y*y,
+      z*z,
+      x*y,
+      y*z,
+      z*x,
+      x,
+      y,
+      z
+      ]).T
+
+    w = np.ones(x.shape)
+    fit, r = np.linalg.lstsq(data,w)[0:2]
+
+    #rms_res = np.sqrt(r / len(w))[0]
+    lin_res = (w - np.dot(data, fit)).sum() / len(w)
+    rms_res = np.sqrt(((w - np.dot(data, fit))**2).sum() / len(w))
+
+    # now solve for z in terms of x and y to evaluate
+    A,B,C,D,E,F,G,H,I = fit
+    a = C
+    b = E*yy + F*xx + I
+    c = A*xx**2 + B*yy**2 + D*xx*yy + G*xx + H*yy - 1
+    zz = (-b + np.sqrt(b**2 - 4*a*c)) / (2*a)
 
   # fill the calibration matrix with values from fit
   dest[yy,xx] = zz
 
   return lin_res, rms_res
 
-def calibrate(info):
+def calibrate(info, fit_type=FIT_QUADRATIC):
   # load exposure files
   exposures = [Exposure(f) for f in info.exposure_files]
 
@@ -164,7 +196,7 @@ def calibrate(info):
 
   # fit smooth shape for each crystal and fill in calib with fit
   for xtal in info.xtals:
-    lr, rr = fit_xtal(xtal, points, calib)
+    lr, rr = fit_xtal(xtal, points, calib, fit_type)
     lin_res.append(lr)
     rms_res.append(rr)
 

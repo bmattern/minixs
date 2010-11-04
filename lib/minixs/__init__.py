414944526584977ce883a54f3f5fab154d75696b
Brian Mattern <rephorm@rephorm.com>
2010-11-03 20:44:51 -0700

    rethink what gets imported

--- lib/minixs/__init__.py
+++ lib/minixs/__init__.py
@@ -1,1 +1,21 @@
-from minixs import *
+import calibrate, \
+       emission, \
+       exposure, \
+       filter, \
+       info, \
+       misc, \
+       rixs, \
+       scanfile
+
+from constants import *
+
+__all__ = [
+  'calibrate',
+  'emission',
+  'exposure',
+  'filter',
+  'info',
+  'misc',
+  'rixs',
+  'scanfile',
+  ]

fd29bd832d79d3ef284488c3ab30eda33d9792c7
Brian Mattern <bmattern@uw.edu>
2011-03-22 16:00:23 -0700

    add validation

--- lib/minixs/gui/processor/main.py
+++ lib/minixs/gui/processor/main.py
@@ -5,13 +5,23 @@ from const import *
 from minixs import filter, emission
 from minixs.gui import filter_view
 
+class ProcessorModel(emission.EmissionSpectrum):
+  def validate(self):
+    self.validation_errors = []
+    if len(self.exposure_files) < 1:
+      self.validation_errors.append("No exposure files have been selected.")
+    if not self.calibration_file:
+      self.validation_errors.append("No calibration matrix has been specified.")
+
+    return len(self.validation_errors) == 0
+
 class ProcessorApp(wx.App):
   def __init__(self, *args, **kwargs):
     wx.App.__init__(self, *args, **kwargs)
     self.SetAppName('minixs')
 
     v = view.ProcessorView(None, ID_MAIN_FRAME, 'miniXS XES Processor')
-    m = emission.EmissionSpectrum()
+    m = ProcessorModel()
     c = controller.ProcessorController(v, m)
     
     v.Show()

4b64a9a5fd29e5474e30fc572d0fa282b7053274
Brian Mattern <bmattern@uw.edu>
2011-07-13 09:51:15 -0700

    fix save to folder other than current

--- lib/minixs/gui/file_dialog.py
+++ lib/minixs/gui/file_dialog.py
@@ -25,12 +25,13 @@ def FileDialog(parent, dirlist, type, title, wildcard='', save=False, multiple=F
 
   paths = []
   if ret == wx.ID_OK:
-    directory = dlg.GetDirectory()
+    paths = dlg.GetPaths()
+    paths.sort()
+    if (paths):
+      directory = os.path.dirname(paths[0])
+    else:
+      directory = dlg.GetDirectory()
     dirlist[type] = dirlist['last'] = directory
-    filenames = dlg.GetFilenames()
-    filenames.sort()
-
-    paths = [os.path.join(directory, f) for f in filenames]
 
   dlg.Destroy()
 

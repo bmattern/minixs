eabb09efb1278324b3a26bed8a0ef1fab71e555a
Brian Mattern <bmattern@uw.edu>
2011-09-05 22:37:48 -0700

    make progess dialog bigger

--- lib/minixs/gui/progress.py
+++ lib/minixs/gui/progress.py
@@ -8,5 +8,9 @@ class WxProgressIndicator(ProgressIndicator):
     self.dialog = wx.ProgressDialog(title, name, maximum=100, style=wx.PD_APP_MODAL | wx.PD_AUTO_HIDE)
     self.dialog.Show()
 
+    w,h = self.dialog.GetSize()
+    self.dialog.SetSize((300, h))
+
+
   def do_update(self):
     self.dialog.Update(int(self.progress * 100), self.msg)

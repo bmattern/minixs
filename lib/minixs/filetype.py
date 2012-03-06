2dcdbc3b5a9c64a10a7b0358f8333c4623054f9f
Brian Mattern <bmattern@uw.edu>
2012-03-05 18:17:35 -0800

    better filetype determination

--- lib/minixs/filetype.py
+++ lib/minixs/filetype.py
@@ -1,32 +1,42 @@
-FILE_UNKNOWN     = -1
-FILE_CALIBRATION = 0
-FILE_XTALS       = 1
-FILE_XES         = 2
-FILE_RIXS        = 3
+FILE_UNKNOWN     = 0
+FILE_CALIBRATION = 1
+FILE_XTALS       = 2
+FILE_XES         = 3
+FILE_RIXS        = 4
 
-FILE_TYPES = [
-    'calibration matrix',
-    'crystal boundaries',
-    'XES spectrum',
-    'RIXS spectrum'
-    ]
+FILE_TYPES = {
+    'calibration matrix': FILE_CALIBRATION,
+    'crystal boundaries': FILE_XTALS,
+    'xes spectrum': FILE_XES,
+    'rixs spectrum': FILE_RIXS,
+    'emission spectrum': FILE_XES,
+    }
+
 
 def determine_filetype(path):
   with open(path) as f:
     line = f.readline()
-    if len(line) == 0 or line[0] != "#":
-      return FILE_UNKNOWN
+    return determine_filetype_from_header(line)
 
-    s = line[2:]
-
-    if not s.lower().startswith('minixs'):
-      return 0
-
-    t = s[7:].strip()
-    if t in FILE_TYPES:
-      return FILE_TYPES.index(t)
-
+def determine_filetype_from_header(header):
+  if len(header) == 0 or header[0] != "#":
     return FILE_UNKNOWN
+
+  s = header[2:].strip().lower()
+
+  valid_prefix = False
+
+  # check prefix of header from most recent to oldest
+  for pref in ["minixes", "minixs"]:
+    if s.startswith(pref):
+      s = s[len(pref):].strip()
+      valid_prefix = True
+      break
+
+  if not valid_prefix:
+    return FILE_UNKNOWN
+
+  return FILE_TYPES.get(s, FILE_UNKNOWN)
 
 class InvalidFileError(Exception):
   pass

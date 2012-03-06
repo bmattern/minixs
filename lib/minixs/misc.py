b19d522ca3814c0445377a277a7a8b532c359afb
Brian Mattern <bmattern@uw.edu>
2012-03-05 18:45:32 -0800

    remove cruft from misc, cleanup plot_rixs script

--- lib/minixs/misc.py
+++ lib/minixs/misc.py
@@ -70,6 +70,14 @@ def build_rixs(spectra, energies):
 
   return full_spectrum
 
+def save_rixs(filename, rixs):
+  with open(filename, 'w') as f:
+    f.write("#    E_incident      E_emission       Intensity           Sigma          Counts      Num_pixels\n")
+
+    fmt = ('% 15.2f', '% 15.2f', '% 15.8e','% 15.8e','% 15d', '% 15d')
+    np.savetxt(f, rixs, fmt=fmt)
+
+
 def rixs_xes_cut(rixs, energy):
   energies = np.unique(rixs[:,0])
  
@@ -83,71 +91,6 @@ def rixs_pfy_cut(rixs, energy):
   i = np.argmin(np.abs(energies - energy))
 
   return rixs[np.where(rixs[:,1] == energies[i])]
-
-
-def rixs2d(rixs):
-  inc_energies = np.unique(rixs[:,0])
-
-  i = len(inc_energies)
-  return rixs[:,2].reshape((i,len(rixs)/i)).T
-
-def plot_rixs_contour(rixs, plot_log=False, aspect=1):
-  incE = np.unique(rixs[:,0])
-  emitE = np.unique(rixs[:,1])
-  r2d = rixs2d(rixs)
-  if plot_log:
-    r2d = np.log10(r2d)
-
-  from matplotlib.pyplot import contourf, figure, colorbar
-  figure()
-  contourf(incE, emitE, r2d, 50, aspect=aspect)
-  colorbar()
-
-def plot_rixs(rixs, start=0, end=-1, plot_log=False, aspect=1):
-  incE = np.unique(rixs[:,0])
-  emitE = np.unique(rixs[:,1])
-  r2d = rixs2d(rixs)
-  if plot_log:
-    r2d = np.log10(r2d)
-
-  from matplotlib.pyplot import imshow, figure
-  figure()
-  imshow(
-      r2d[:,start:end],
-      extent=(incE[start],incE[end],emitE[-1],emitE[0]),
-      aspect=aspect)
-
-def plot_spectrum(s, **kwargs):
-  plot_errorbars = True
-  if kwargs.has_key('errorbar'):
-    plot_errorbars = kwargs['errorbar']
-    del(kwargs['errorbar'])
-
-  new_plot = True
-  if kwargs.has_key('new_plot'):
-    new_plot = kwargs['new_plot']
-    del(kwargs['new_plot'])
-
-  normalize = False
-  if kwargs.has_key('normalize'):
-    normalize = kwargs['normalize']
-    del(kwargs['normalize'])
-
-
-  from matplotlib.pyplot import plot, errorbar, figure
-
-  if new_plot:
-    figure()
-
- 
-  n = 1
-  if normalize:
-    n = np.sum(s[:,1]) / len(s[:,1])
-
-  if plot_errorbars:
-    errorbar(s[:,0], s[:,1]/n, s[:,2]/n, **kwargs)
-  else:
-    plot(s[:,0], s[:,1]/n, **kwargs)
 
 def _find_boundaries(p, axis):
   """

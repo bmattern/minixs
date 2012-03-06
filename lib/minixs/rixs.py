9856d02fc427f7d876397df80c757de2974ea93b
Brian Mattern <bmattern@uw.edu>
2012-03-05 18:21:30 -0800

    Use parser for RIXS load. save/load filter list.

--- lib/minixs/rixs.py
+++ lib/minixs/rixs.py
@@ -2,8 +2,19 @@ import minixs as mx
 from emission import process_spectrum
 import numpy as np
 from itertools import izip
+from parser import Parser, FLOAT, STRING, LIST
+
+class InvalidParameters(Exception): pass
 
 class RIXS(object):
+  """
+  A RIXS Spectrum is a 2D spectrum of intensity vs incident and emitted photon energies.
+
+  In the miniXES context, this is simply a set of emission spectra taken at varying incident energies.
+  This class takes a list of energies, incident fluxes (I0) and Pilatus exposures and converts into
+  a normalized RIXS spectrum by processing each exposure in turn and dividing the resulting intensities
+  by the corresponding I0 value.
+  """
   def __init__(self, filename=None):
     self.dataset_name = ""
     self.calibration_file = ""
@@ -75,36 +86,19 @@ class RIXS(object):
       pos = f.tell()
       line = f.readline()
 
-      in_exposures = False
+      header = []
+
+      parser = Parser({
+        'Spectrometer': STRING,
+        'Dataset': STRING,
+        'Calibration File': STRING,
+        'Incident Energies / I0s / Exposures': (LIST, (FLOAT, FLOAT, STRING)),
+        'Filters': (LIST, STRING),
+        })
 
       while line:
         if line[0] == "#":
-
-          # parse exposures
-          if in_exposures:
-            bits = line[2:].split(None, 3)
-            if not bits:
-              in_exposures = False
-            elif len(bits) > 0:
-              energy = float(bits[0].strip())
-              I0 = float(bits[1].strip())
-              ef = ' '.join(bits[2:]).strip()
-              if ef:
-                self.energies.append(energy)
-                self.I0s.append(I0)
-                self.exposure_files.append(ef)
-            else:
-              raise Exception("Invalid Format")
-
-          elif line[2:10] == 'Dataset:':
-            self.dataset_name = line[11:].strip()
-          elif line[2:19] == 'Calibration File:':
-            self.calibration_file = line[20:].strip()
-          elif line[2:38] == 'Incident Energies / I0s / Exposures:':
-            self.exposure_files = []
-            in_exposures = True
-          else:
-            pass
+          header.append(line[2:])
         elif header_only:
           return
         else:
@@ -116,16 +110,78 @@ class RIXS(object):
         pos = f.tell()
         line = f.readline()
 
+    parsed = parser.parse(header)
+    if parser.errors:
+      self.load_errors += parser.errors
+
+    self.dataset_name = parsed.get('Dataset', '')
+
+    exposure_info = parsed.get('Incident Energies / I0s / Exposures', [])
+    self.energies = [ei[0] for ei in exposure_info]
+    self.I0s = [ei[1] for ei in exposure_info]
+    self.exposure_files = [ei[2] for ei in exposure_info]
+
+    self.calibration_file = parsed.get('Calibration', None)
+
+    self.filters = []
+    for filter_line in parsed.get('Filters', []):
+      name,val = filter_line.split(':')
+      name = name.strip()
+      fltr = mx.filter.get_filter_by_name(name)
+      if fltr == None:
+        self.load_errors.append("Unknown Filter: '%s' (Ignoring)" % name)
+      else:
+        fltr.set_str(val.strip())
+        self.filters.append(fltr)
+
+  def _validate_before_processing(self):
+    self.errors = []
+
+    if not self.calibration_file:
+      self.errors.append("Missing calibration_file.")
+    if not self.energies:
+      self.errors.append("Incident energies have not been set.")
+    if not self.I0s:
+      self.errors.append("I0 values have not been set.")
+    if not self.exposure_files:
+      self.errors.append("Exposure files have not been set.")
+    if not (len(self.exposure_files) == len(self.I0s) == len(self.exposure_files)):
+      self.errors.append("The number of exposures, energies and I0 values are not all the same.")
+
+    return len(self.errors) == 0
+
+
   def process(self, emission_energies=None, progress_callback=None, skip_columns=[]):
+    """
+    Process this RIXS spectrum
+
+    Preconditions:
+      self.calibration_file is set to calibration filename
+      self.exposure_files is list of exposure filenames
+      self.energies is list of incident energies corresponding to exposure_files
+      self.I0s is list of incident fluxes corresponding to exposure_files
+
+    Parameters:
+      emission_energies - list of points for emission energy grid
+      progress_callback - callback to call after each exposure is processed
+      skip_columns - list of columns (for vertical dispersive dir) or rows (for horizontal) to skip entirely
+
+    Raises:
+      InvalidParameters if any of the preconditions is not met. In this case, self.errors is set to a list of human readable error messages.
+    """
+    if not self._validate_before_processing():
+      raise InvalidParameters()
+
     calibration = mx.calibrate.Calibration()
-    calibration.load(self.calibration_file)
+
+    if not calibration.load(self.calibration_file):
+      self.errors.append("Invalid calibration file:\n  " + "\n  ".join(calibration.load_errors))
+      raise InvalidParameters()
 
     # generate emission energies from range of calibration matrix
     if emission_energies is None:
-      Emin = calibration.calibration_matrix[np.where(calibration.calibration_matrix > 0)].min()
-      Emax = calibration.calibration_matrix.max()
-
-      emission_energies = np.arange(Emin,Emax,.1)
+      Emin,Emax = calibration.energy_range()
+      emission_energies = np.arange(Emin,Emax,0.1)
 
     # create rixs array
     stride = len(emission_energies)
@@ -155,6 +211,16 @@ class RIXS(object):
     self.spectrum = spectrum
 
   def matrix_form(self):
+    """
+    Convert to a 2D matrix of intensities
+
+    Returns:
+      (incident, emitted, spectrum)
+
+      incident - incident energy values (x-axis)
+      emitted - emitted energy values (y-axis)
+      spectrum - 2d array of intensities
+    """
     inc_energies = np.unique(self.spectrum[:,0])
     emit_energies = np.unique(self.spectrum[:,1])
 
